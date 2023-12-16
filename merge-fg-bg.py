9d68ab89f6fe967324d61e777752c7a63c5e15e4
Leandro Sitorus <leandrositorus@Leandros-MacBook-Air-2.local>
2023-12-16 18:45:23 +0700

    Add notebook

--- merge-fg-bg.py
+++ merge-fg-bg.py
@@ -10,7 +10,7 @@ gray = cv2.cvtColor(foreground, cv2.COLOR_BGR2GRAY)
 
 _, mask = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
 
-# figure out to now crop the background, but resize the foreground (product image) to fit into the background
+# figure out to not crop the background, but resize the foreground (product image) to fit into the background
 crop_background = cv2.resize(background, (foreground.shape[1], foreground.shape[0]), interpolation = cv2.INTER_NEAREST)
 _, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)
 mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
