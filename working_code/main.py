f5141e76f1c9e8b92d48d275e12aae6c69102d26
Romual Sitorus <romual.sitorus@gdn-commerce.com>
2023-12-19 02:54:39 +0700

    Update

--- working_code/main.py
+++ working_code/main.py
@@ -1,3 +1,3 @@
 from rundalle import generate_ai
 
-print(generate_ai('Nike-Air-Max-90-DX0115-100-5.webp', 'A shoe sitting on top of a table.', 'Nike', 'Air Force 1', 'Rp8.499.000'))
+print(generate_ai('ath-earbuds-r.jpeg', 'A office table with books and other office appliances', 'Nike', 'Air Force 1', 'Rp8.499.000'))
